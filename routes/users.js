936d647cd698df10e545c22c631e6d3ed91ea0e9
cacaudev <cacauabreu2013@gmail.com>
2018-01-27 18:32:18 -0200

    User Model & Register

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,25 @@
-const express = require('express');
-const router  = express.Router();
+const express  = require('express');
+const passport = require('passport');
+const jwt      = require('jsonwebtoken');
+const User     = require('../models/user');
+const router   = express.Router();
 
-router.get('/register', (req, res, next) => {
-  res.send('REGISTER');
+router.post('/register', (req, res, next) => {
+  let newUser = new User({
+    name: req.body.name,
+    email: req.body.email,
+    username: req.body.username,
+    password: req.body.password
+  });
+
+  User.addUser(newUser, (err, user) => {
+    if(err) {
+      res.json({sucess: false, msg: 'Failed to register user.'});
+    } else {
+      res.json({sucess: true, msg: 'User registered.'});
+    }
+  });
+
 });
 
 router.post('/authenticate', (req, res, next) => {
@@ -12,11 +29,6 @@ router.post('/authenticate', (req, res, next) => {
 router.get('/profile', (req, res, next) => {
   res.send('PROFILE');
 });
-
-router.get('/validate', (req, res, next) => {
-  res.send('VALIDATE');
-});
-
 
 module.exports = router;
 
